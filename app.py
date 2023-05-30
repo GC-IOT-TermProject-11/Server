92951aac5eba880a9b1e2f828c32e864b2948dc4
GYUWON-JUNG <licmm0520@gmail.com>
2023-05-30 21:31:23 +0900

    유저앱과 동일한 JSON Key 값으로 변경

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
-import joblib
+import joblib, csv
 import pandas as pd
 
 app = Flask(__name__)
 model = joblib.load('model.joblib')
 base_csv_file = 'base.csv'
+
 
 @app.route('/')
 def hello():
@@ -14,23 +15,33 @@ def hello():
 def predict():
     # 클라이언트로부터 전달된 와이파이 리스트 가져오기
     data = request.get_json()
-    wifi_list = data.get('wifi_list', [])
+    wifi_list = data.get('wifiList', [])
 
     # 예측을 위해 입력 데이터 형식 변환
     input_data = []
     for wifi in wifi_list:
-        bssid = wifi.get('bssid')
-        rssi = wifi.get('rssi')
+        bssid = wifi.get('BSSID')
+        rssi = wifi.get('RSSI')
+        app.logger.info('BSSID: %s, RSSI: %s', bssid, rssi)  # 클라이언트에서 받은 값을 로그로 출력
         input_data.append([bssid, rssi])
+
+    with open('test.csv', 'w') as file:
+        write = csv.writer(file)
+        write.writerow(input_data)
 
     # base.csv 파일을 데이터프레임으로 읽어오기
     df = pd.read_csv(base_csv_file)
 
-     # BSSID와 받아온 값 매핑
+    # BSSID와 받아온 값 매핑
     for received_value in input_data:
         bssid, rssi = received_value
-        if bssid in df.columns:
-            df.loc[df['BSSID'] == bssid, 'RSSI'] = rssi  # 값 교체
+        # BSSID 값이 일치하는 열의 인덱스를 찾기
+        matching_columns = df.columns[df.columns == bssid]
+
+        # 일치하는 BSSID 값이 존재하는 경우에만 RSSI 값을 변경
+        if len(matching_columns) > 0:
+            column_index = matching_columns[0]
+            df[column_index] = rssi
 
     # 모델에 예측 입력 데이터 전달
     X = df.drop(columns=['room'])
@@ -42,8 +53,8 @@ def predict():
     response = {'predictions': predictions}
 
     # 예측 결과를 클라이언트에 반환
-
     return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
